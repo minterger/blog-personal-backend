85f7eedf8494904ef294148faad9bd3185a71611
Leonel Menci <minterger@gmail.com>
2023-05-01 16:38:05 -0300

    add Post create, update and delete

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -10,4 +10,81 @@ module.exports = {
       console.error(error);
     }
   },
+
+  async createPosts(req, res) {
+    try {
+      const { title, body, url, hidden } = req.body;
+      const author = req.user.firstName + " " + req.user.lastName;
+
+      const newPost = new Post({
+        author,
+        title,
+        body,
+        url,
+        hidden,
+      });
+
+      const savedPost = await newPost.save();
+
+      res.json({
+        post: savedPost,
+        message: "Post creado con exito",
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+
+  async editPost(req, res) {
+    try {
+      const { title, body, url, hidden } = req.body;
+      const id = req.params.id;
+
+      const post = await Post.findById(id);
+
+      if (!post) {
+        res.status(401).json({
+          message: "Post no encontrado",
+        });
+      }
+
+      post.title = title;
+      post.body = body;
+      post.hidden = hidden;
+      post.url = url;
+
+      const editedPost = await post.save();
+
+      res.json({
+        post: editedPost,
+        message: "Post creado con exito",
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+
+  async deletePost(req, res) {
+    try {
+      const id = req.params.id;
+      const post = await Post.findById(id);
+
+      if (!post) {
+        res.status(401).json({
+          message: "Post no encontrado",
+        });
+      }
+
+      await Post.deleteOne({ id }).populate("comments");
+
+      res.json({
+        message: "Post eliminado",
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
 };
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,8 +1,21 @@
 const { Router } = require("express");
-const { getPosts } = require("../controllers/index.controller");
+const {
+  getPosts,
+  createPosts,
+  editPost,
+  deletePost,
+} = require("../controllers/index.controller");
+
+const { checkUser, userIsAdmin } = require("../middlewares/checkUser");
 
 const route = Router();
 
 route.get("/posts", getPosts);
+
+route.post("/posts", checkUser, userIsAdmin, createPosts);
+
+route.put("/posts/:id", checkUser, userIsAdmin, editPost);
+
+route.delete("/posts/:id", checkUser, userIsAdmin, deletePost);
 
 module.exports = route;
