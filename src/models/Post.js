c9b6d0eada8f3b9cbd148608b9916648ea204aa9
Leonel Menci <minterger@gmail.com>
2023-05-01 16:36:00 -0300

    add url item and modify required item

--- src/models/Post.js
+++ src/models/Post.js
@@ -2,11 +2,13 @@ const { model, Schema } = require("mongoose");
 
 const postSchema = new Schema(
   {
-    title: String,
-    author: String,
-    body: String,
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    body: { type: String, required: true },
+    url: { type: String, required: true },
+
     comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-    hidden: Boolean,
+    hidden: { type: Boolean, default: false },
   },
   {
     timestamps: true,
